1d440008d23db85d93f5791b0dd241c835fd22b6
haganvrazel <vrazel97@yahoo.com>
2020-10-21 09:41:08 -0500

    added getFullName

--- Functions/parameterized_functions.js
+++ Functions/parameterized_functions.js
@@ -1,6 +1,12 @@
 function sayHello(name) {
     console.log("Hello " + name)
+    console.log(`Hello ${name}`)
+}
+
+function getFullName(firstname, lastname) { 
+    console.log(`${firstname} ${lastname}`)
 }
 
 sayHello("King");
 sayHello("Hagan");
+getFullName('Hagan', 'Vrazel')
