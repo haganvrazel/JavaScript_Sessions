b2b7fcef65bb6d7a8ea6a8bd633fa6eaf255bee5
haganvrazel <vrazel97@yahoo.com>
2020-10-21 10:01:25 -0500

    getElementById

--- Functions/parameterized_functions.js
+++ Functions/parameterized_functions.js
@@ -7,6 +7,12 @@ function getFullName(firstname, lastname) {
     console.log(`${firstname} ${lastname}`)
 }
 
+function getFullNameAgain() {
+    var fName = document.getElementById('firstName').value
+    var lName = document.getElementById('lastName').value
+    alert(`${fName} ${lName}`)
+}
+
 // sayHello("King");
 // sayHello("Hagan");
 // getFullName('Hagan', 'Vrazel')
